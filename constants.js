7568187f4aca8589cdf8da1716c5ff6122218ae6
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-12-19 17:19:34 +0100

    init commit | working

---
+++ constants.js
@@ -0,0 +1,17 @@
+// const tmp = 8080;
+const tmp = 3306;
+
+const DATABASE_NAME = "demo";
+const USERNAME = "root";
+const PASSWORD = "";
+const HOST = "127.0.0.1";
+const DIALECT = "mysql";
+const PORT = tmp;
+module.exports = {
+  DATABASE_NAME: DATABASE_NAME,
+  USERNAME: USERNAME,
+  PASSWORD: PASSWORD,
+  HOST: HOST,
+  DIALECT: DIALECT,
+  PORT: PORT
+};
